4f3a18d12bf93fa180e63aaaca8fa9d85de67f67
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2025-11-20 11:16:16 +0300

    first_commit

---
+++ models.py
@@ -0,0 +1,11 @@
+import openai
+import os
+from typing import Optional
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger("AI_Travel")
+
